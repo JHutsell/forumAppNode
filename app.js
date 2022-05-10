e9819d5e40d570690802fc9ebc072721fc159d5a
Justin Hutsell <45610398+JHutsell@users.noreply.github.com>
2022-05-10 16:14:30 -0500

    adds router, to move routing functions out of our main app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
+const router = require('./router')
+// whatever is exported is stored in router variable
+
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('home-guest')
-})
+app.use('/', router)
 
 app.listen(3000)
