159373f0d9a55eceaaf339408803f8bc3105382b
Justin Hutsell <45610398+JHutsell@users.noreply.github.com>
2022-05-12 18:02:55 -0500

    adds ability for users to login, utilizing js Promise and .then() functions

--- models/User.js
+++ models/User.js
@@ -8,8 +8,8 @@ let User = function(data) {
 
 User.prototype.cleanUp = function() {
   if (typeof(this.data.username) != "string") {this.data.username = ""}
-  if (typeof(this.data.username) != "string") {this.data.email = ""}
-  if (typeof(this.data.username) != "string") {this.data.password = ""}
+  if (typeof(this.data.email) != "string") {this.data.email = ""}
+  if (typeof(this.data.password) != "string") {this.data.password = ""}
 
   // get rid of extra properties
   this.data = {
@@ -30,6 +30,21 @@ User.prototype.validate = function() {
   if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
   if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least three characters.")}
   if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
+}
+
+User.prototype.login = function() {
+  return new Promise((resolve, reject) => {
+    this.cleanUp()
+    usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
+      if (attemptedUser && attemptedUser.password == this.data.password) {
+        resolve("Congrats")
+      } else {
+        reject("Invalid username/password")
+      }
+    }).catch(function() {
+      reject("Please try again later.")
+    })
+  })
 }
 
 User.prototype.register = function() {
