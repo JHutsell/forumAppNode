66fd6e0db3a6bd8c6dd44f7589fe74cf140b5027
Justin Hutsell <45610398+JHutsell@users.noreply.github.com>
2022-05-12 18:23:24 -0500

    adds bcryptjs package to hash users passwords in the database

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const usersCollection = require('../db').collection('users')
 const validator = require('validator')
 
@@ -25,7 +26,7 @@ User.prototype.validate = function() {
   if (this.data.password == "") {this.errors.push("You must provide a password.")}
 
   if (this.data.password.length > 0 && this.data.password.length < 8) {this.errors.push("Password must be at least eight characters.")}
-  if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
+  if (this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters.")}
 
   if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
   if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least three characters.")}
@@ -36,7 +37,7 @@ User.prototype.login = function() {
   return new Promise((resolve, reject) => {
     this.cleanUp()
     usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
-      if (attemptedUser && attemptedUser.password == this.data.password) {
+      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
         resolve("Congrats")
       } else {
         reject("Invalid username/password")
@@ -53,6 +54,9 @@ User.prototype.register = function() {
   this.validate()
   // 2) only if there are no validation errors, save user data into databse
   if (!this.errors.length) {
+    // hash user password
+    let salt = bcrypt.genSaltSync(10)
+    this.data.password = bcrypt.hashSync(this.data.password, salt)
     usersCollection.insertOne(this.data)
   }
 }
