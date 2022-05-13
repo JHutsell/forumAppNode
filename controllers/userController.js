5255fb3b109c864d6d83ae539fe22d7f40f4eb56
Justin Hutsell <45610398+JHutsell@users.noreply.github.com>
2022-05-13 18:37:44 -0500

    adds validation so that new users may not register with duplicate usernames or emails

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,18 +24,26 @@ exports.logout = (req, res) => {
 
 exports.register = (req, res) => {
   let user = new User(req.body)
-  user.register()
-  if (user.errors.length) {
-    res.send(user.errors)
-  } else {
-    res.send("Congrats, no errors")
-  }
+  user.register().then(() => {
+    req.session.user = {username: user.data.username}
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  }).catch((regErrors) => {
+    regErrors.forEach((error) => {
+      req.flash('regErrors', error)
+    })
+    req.session.save(() => {
+      res.redirect('/')
+    })
+  })
+  
 }
 
 exports.home = (req, res) => {
   if (req.session.user) {
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest', {errors: req.flash('errors')})
+    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
 }
