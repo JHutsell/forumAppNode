93b40367b309b4aff495485e3e913c1cd1182c2d
Justin Hutsell <45610398+JHutsell@users.noreply.github.com>
2022-05-15 19:31:02 -0500

    adds users posts to their profile page by querying the database for all posts associated with that user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const Post = require('../models/Post')
 
 exports.mustBeLoggedIn = (req, res, next) => {
   if (req.session.user) {
@@ -69,8 +70,14 @@ exports.ifUserExists = (req, res, next) => {
 }
 
 exports.profilePostsScreen = (req, res) => {
-  res.render('profile', {
-    profileUsername: req.profileUser.username,
-    profileAvatar: req.profileUser.avatar
+  //ask post model for posts by certain author id
+  Post.findByAuthorId(req.profileUser._id).then((posts) => {
+    res.render('profile', {
+      posts: posts,
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar
+    })
+  }).catch(() => {
+    res.render('404')
   })
 }
